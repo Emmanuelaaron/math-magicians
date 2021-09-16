30ab1461ea661ec66dd3558eac011ca580ad34b2
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-09-17 00:20:24 +0300

    Adds interactiveness to the calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/destructuring-assignment */
-/* eslint-disable  react/prop-types */
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './calculator.css';
@@ -10,8 +8,14 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      total: null,
+      next: null,
     };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler(e) {
+    this.setState((state) => calculate(state, e.target.innerHTML));
   }
 
   render() {
@@ -19,29 +23,28 @@ class Calculator extends React.Component {
     const yellowButton = `${uniformStyles} bg-warning`;
     return (
       <div className="row border w-50">
-        <div className="col-12 div height border d-flex justify-content-end align-items-end bg-secondary text-light" role="button">{this.props.total}</div>
-        <div className={uniformStyles} role="button">AC</div>
-        <div className={uniformStyles} role="button">+/-</div>
-        <div className={uniformStyles} role="button">%</div>
-        <div className={yellowButton} role="button">÷</div>
-        <div className={uniformStyles} role="button">7</div>
-        <div className={uniformStyles} role="button">8</div>
-        <div className={uniformStyles} role="button">9</div>
-        <div className={yellowButton} role="button">X</div>
-        <div className={uniformStyles} role="button">4</div>
-        <div className={uniformStyles} role="button">5</div>
-        <div className={uniformStyles} role="button">6</div>
-        <div className={yellowButton} role="button">-</div>
-        <div className={uniformStyles} role="button">1</div>
-        <div className={uniformStyles} role="button">2</div>
-        <div className={uniformStyles} role="button">3</div>
-        <div className={yellowButton} role="button">+</div>
-        <div className="col-6 div height border d-flex justify-content-center align-items-center" role="button">0</div>
-        <div className={uniformStyles} role="button">.</div>
-        <div className={yellowButton} role="button">=</div>
+        <div className="col-12 div height border d-flex justify-content-end align-items-end bg-secondary text-light">{this.state.next || this.state.total}</div>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>AC</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>+/-</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>%</button>
+        <button type="button" className={yellowButton} onClick={this.clickHandler}>÷</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>7</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>8</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>9</button>
+        <button type="button" className={yellowButton} onClick={this.clickHandler}>x</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>4</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>5</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>6</button>
+        <button type="button" className={yellowButton} onClick={this.clickHandler}>-</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>1</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>2</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>3</button>
+        <button type="button" className={yellowButton} onClick={this.clickHandler}>+</button>
+        <button type="button" className="col-6 div height border d-flex justify-content-center align-items-center" onClick={this.clickHandler}>0</button>
+        <button type="button" className={uniformStyles} onClick={this.clickHandler}>.</button>
+        <button type="button" className={yellowButton} onClick={this.clickHandler}>=</button>
       </div>
     );
   }
 }
-Calculator.defaultProps = { total: 0 };
 export default Calculator;
